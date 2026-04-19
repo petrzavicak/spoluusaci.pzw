a2641c69f68dee20846b9ffa08d56e7f61824e68
Vít Mikušek <funshar@Vits-MacBook-Air.local>
2026-04-19 22:33:06 +0200

    commit

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-        <title>{{ $title ?? 'Naše Svatba' }}</title>
+        <title>{{ $title ?? 'Svatba ušáci' }}</title>
 
         <!-- Google Fonts pro lepší čitelnost a styl -->
         <link rel="preconnect" href="https://fonts.googleapis.com">
